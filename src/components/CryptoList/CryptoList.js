4f6b17f947ed48a7a51327980738235d4b541393
Czajka <krzysztof.czajka@forum-media.pl>
2022-01-03 11:51:45 +0100

    Style for CryptoList component

--- src/components/CryptoList/CryptoList.js
+++ src/components/CryptoList/CryptoList.js
@@ -1,10 +1,20 @@
 import React, { useEffect, useState } from 'react';
-import { DataGrid } from '@mui/x-data-grid';
 import axios from 'axios';
+import clsx from 'clsx';
+import { DataGrid } from '@mui/x-data-grid';
+import Box from '@mui/material/Box';
 import { CryptoListItemName } from "./CryptoList.styles";
 
 const CryptoList = () => {
   const [ cryptoList, setCryptoList ] = useState([]);
+  const currencyFormatter = new Intl.NumberFormat('en-US', {
+    style: 'currency',
+    currency: 'USD',
+  });
+  const usdPrice = {
+    type: 'number',
+    valueFormatter: ({ value }) => currencyFormatter.format(Number(value)),
+  };
 
   const columns = [
     { field: 'index', headerName: '#', width: 50 },
@@ -19,9 +29,21 @@ const CryptoList = () => {
         </CryptoListItemName>
       ),
     },
-    { field: 'current_price', headerName: 'Price', width: 100},
-    { field: 'price_change_percentage_24h', headerName: '24h %', width: 100 },
-    { field: 'market_cap', headerName: 'Market Cap', width: 150 }
+    { field: 'current_price', headerName: 'Price', width: 120, ...usdPrice },
+    {
+      field: 'price_change_percentage_24h',
+      headerName: '24h %',
+      headerClassName: 'MuiDataGrid-columnHeader--alignRight',
+      cellClassName: (params) => {
+        return clsx('MuiDataGrid-cell--textRight', {
+          negative: params.value.slice(0, -1) < 0,
+          positive: params.value.slice(0, -1) > 0,
+        });
+      },
+      width: 100,
+      valueGetter: ({ value }) => value && value.toFixed(2) + '%',
+    },
+    { field: 'market_cap', headerName: 'Market Cap', width: '180', ...usdPrice }
   ];
   const rows = cryptoList;
 
@@ -43,7 +65,7 @@ const CryptoList = () => {
               id: item.id,
               image: item.image,
               name: item.name,
-              current_price: item.current_price + '$',
+              current_price: item.current_price,
               price_change_percentage_24h: item.price_change_percentage_24h,
               market_cap: item.market_cap
             };
@@ -66,15 +88,30 @@ const CryptoList = () => {
   }, []);
 
   return (
-      <div style={{ height: '80vh', width: '100%' }}>
-      {/*{console.log(cryptoList)}*/}
+    <Box
+      sx={{
+        height: '80vh',
+        width: '100%',
 
+        '.MuiDataGrid-cell': {
+          fontWeight: 'bold',
+
+          '&.positive': {
+            color: (theme) => theme.palette.success.light,
+          },
+
+          '&.negative': {
+            color: (theme) => theme.palette.error.main,
+          }
+        },
+      }}
+    >
       <DataGrid
         rows={rows}
         columns={columns}
         disableSelectionOnClick
       />
-    </div>
+    </Box>
   );
 };
 
