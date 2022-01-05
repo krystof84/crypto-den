8d707ce9083d0cebd15671ce2fcf5386554f2f9b
Czajka <krzysztof.czajka@forum-media.pl>
2022-01-05 13:18:12 +0100

    Refactor CryptoList and Home component

--- src/components/CryptoList/CryptoList.js
+++ src/components/CryptoList/CryptoList.js
@@ -1,67 +1,8 @@
-import React, { useEffect, useState } from 'react';
-import clsx from 'clsx';
+import React from 'react';
 import { DataGrid } from '@mui/x-data-grid';
 import Box from '@mui/material/Box';
-import { CryptoListItemName } from "./CryptoList.styles";
-import { usdPrice, numberWithCommas, addIndexesObjectsInArray } from "./helpers";
-import { useCrypto } from "../../hooks/useCrypto";
 
-const CryptoList = () => {
-  const [ cryptoList, setCryptoList ] = useState([]);
-  const { getCryptoList } = useCrypto();
-
-  const columns = [
-    { field: 'index', headerName: '#', width: 20 },
-    {
-      field: 'name',
-      headerName: 'Name',
-      width: 200,
-      renderCell: (params) => (
-        <CryptoListItemName>
-          <img src={params.row.image} alt={params.row.name} />
-          <span>{params.row.name}</span>
-        </CryptoListItemName>
-      ),
-    },
-    { field: 'current_price', headerName: 'Price', width: 120, ...usdPrice },
-    {
-      field: 'price_change_percentage_24h',
-      headerName: '24h %',
-      headerClassName: 'MuiDataGrid-columnHeader--alignRight',
-      cellClassName: (params) => {
-        if(params.value) {
-          return clsx('MuiDataGrid-cell--textRight', {
-            negative: params.value.slice(0, -1) < 0,
-            positive: params.value.slice(0, -1) > 0,
-          });
-        }
-      },
-      width: 100,
-      valueGetter: ({ value }) => value && value.toFixed(2) + '%',
-    },
-    { field: 'market_cap', headerName: 'Market Cap', width: '180', ...usdPrice },
-    {
-      field: 'circulating_supply',
-      headerName: 'Circulating supply',
-      width: 220,
-      cellClassName: 'MuiDataGrid-cell--textRight',
-      headerClassName: 'MuiDataGrid-columnHeader--alignRight',
-      renderCell: (params) => (
-        numberWithCommas(params.row.circulating_supply) + ' ' + params.row.symbol.toUpperCase()
-      )
-    },
-  ];
-  const rows = cryptoList;
-
-  useEffect(() => {
-
-    (async() => {
-      const list = await getCryptoList();
-      const newList = addIndexesObjectsInArray(list.data);
-      setCryptoList(newList);
-    })();
-
-  }, [getCryptoList]);
+const CryptoList = ({rows, columns}) => {
 
   return (
     <Box
--- src/views/Home/Home.js
+++ src/views/Home/Home.js
@@ -1,11 +1,72 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
+import clsx from 'clsx';
 import CryptoList from 'components/CryptoList/CryptoList'
+import { CryptoListItemName } from "components/CryptoList/CryptoList.styles";
+import {addIndexesObjectsInArray, numberWithCommas, usdPrice} from "components/CryptoList/helpers";
+import {useCrypto} from "hooks/useCrypto";
+import Box from '@mui/material/Box';
 
 const Home = () => {
+  const [ cryptoList, setCryptoList ] = useState([]);
+  const { getCryptoList } = useCrypto();
+
+  const rows = cryptoList;
+  const columns = [
+    { field: 'index', headerName: '#', width: 20 },
+    {
+      field: 'name',
+      headerName: 'Name',
+      width: 200,
+      renderCell: (params) => (
+        <CryptoListItemName>
+          <img src={params.row.image} alt={params.row.name} />
+          <span>{params.row.name}</span>
+        </CryptoListItemName>
+      ),
+    },
+    { field: 'current_price', headerName: 'Price', width: 120, ...usdPrice },
+    {
+      field: 'price_change_percentage_24h',
+      headerName: '24h %',
+      headerClassName: 'MuiDataGrid-columnHeader--alignRight',
+      cellClassName: (params) => {
+        if(params.value) {
+          return clsx('MuiDataGrid-cell--textRight', {
+            negative: params.value.slice(0, -1) < 0,
+            positive: params.value.slice(0, -1) > 0,
+          });
+        }
+      },
+      width: 100,
+      valueGetter: ({ value }) => value && value.toFixed(2) + '%',
+    },
+    { field: 'market_cap', headerName: 'Market Cap', width: '180', ...usdPrice },
+    {
+      field: 'circulating_supply',
+      headerName: 'Circulating supply',
+      width: 220,
+      cellClassName: 'MuiDataGrid-cell--textRight',
+      headerClassName: 'MuiDataGrid-columnHeader--alignRight',
+      renderCell: (params) => (
+        numberWithCommas(params.row.circulating_supply) + ' ' + params.row.symbol.toUpperCase()
+      )
+    },
+  ];
+
+  useEffect(() => {
+
+    (async() => {
+      const list = await getCryptoList();
+      const newList = addIndexesObjectsInArray(list.data);
+      setCryptoList(newList);
+    })();
+
+  }, [getCryptoList] );
+
   return (
-    <>
-      <CryptoList />
-    </>
+    <Box>
+      <CryptoList rows={rows} columns={columns} />
+    </Box>
   );
 };
 
