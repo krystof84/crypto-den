15c6e8848243a7779a7cd43066271c700f355e62
Czajka <krzysztof.czajka@forum-media.pl>
2022-01-05 14:50:48 +0100

    Add Basic Crypto Category Menu

--- src/components/CryptoList/CryptoList.js
+++ src/components/CryptoList/CryptoList.js
@@ -1,6 +1,7 @@
 import React from 'react';
 import { DataGrid } from '@mui/x-data-grid';
 import Box from '@mui/material/Box';
+import Link from '@mui/material/Link';
 
 const CryptoList = ({rows, columns}) => {
 
@@ -26,6 +27,17 @@ const CryptoList = ({rows, columns}) => {
         },
       }}
     >
+      <Box>
+        Categories:
+        <Link href="/category/meme-token">Mem Tokens</Link>
+        <Link href="/category/decentralized-finance-defi">Defi</Link>
+        <Link href="/category/non-fungible-tokens-nft">NFT</Link>
+        <Link href="/category/metaverse">Metaverse</Link>
+        <Link href="/category/dot-ecosystem">Polkadot</Link>
+        <Link href="/category/play-to-earn">Play to earn</Link>
+        <Link href="/category/solana-ecosystem">Solana</Link>
+        <Link href="/category/avalanche-ecosystem">Avalanche</Link>
+      </Box>
       <DataGrid
         rows={rows}
         columns={columns}
